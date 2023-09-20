fb8bbc400accbd5110efb4aa106fd89bc2f9645b
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-09-20 14:47:03 +0300

    first commit

---
+++ Exercises/Data Types and Variables/07. Lower or Upper.js
@@ -0,0 +1,13 @@
+function lowerOrUpper(params) {
+    
+    let upper = params.toUpperCase();
+
+    if (params === upper) {
+        console.log('upper-case');
+    }else{
+        console.log('lower-case');
+    }
+
+}
+lowerOrUpper('L')
+lowerOrUpper('f')
