32fb649ceba0e1959fe98ee4ea06d94a0a6be0c1
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-09-23 22:29:34 +0300

    Add files via upload

---
+++ Exercises/Data Types and Variables/04. Biggest of 3 Numbers.js
@@ -0,0 +1,14 @@
+function BiggestOf3Numbers(n1,n2,n3) {
+ 
+ if (n1 > n2 && n1 > n3) {
+    console.log(n1);
+ }else if (n2 > n1 && n2 > n3) {
+    console.log(n2);
+ }else{
+    console.log(n3);
+ }
+    
+}
+BiggestOf3Numbers(-2,
+    7,
+    3)
