abb7971982859faf84faf9b35f18bed6b8dc87fa
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-09-24 17:33:26 +0300

    more exercises

--- Exercises/Data Types and Variables/More Exercises/Data Types & Variables/06. Chess Board.js
+++ Exercises/Data Types and Variables/More Exercises/Data Types & Variables/06. Chess Board.js
@@ -1,5 +1,29 @@
-function chessBoard(params) {
-    
+function chessBoard(num) {
 
+    console.log('<div class="chessboard">');
+
+    for (let row = 1; row <= num; row   ++) {
+        console.log('<div>');
+        for (let col = 1; col <= num; col++) {
+
+            if (row % 2 === 0) {
+                if (col % 2 === 0) {
+                    console.log('<span class="black"></span>')
+                }else{
+                    console.log('<span class="white"></span>');
+                }
+                
+            } else {
+                if (col % 2 === 0) {
+                    console.log('<span class="white"></span>');
+                }else{
+                    console.log('<span class="black"></span>')
+                }
+                
+            }
+        }
+        console.log('</div>')
+    }
+    console.log('</div>');
 }
 chessBoard(3)
