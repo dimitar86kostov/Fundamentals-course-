0d1ea3c5f55afa91fdce9d06efba5b7f112c98fb
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-10-12 22:08:33 +0300

    80/100

--- Arrays Advanced/Exercises/02. Distinct Array.js
+++ Arrays Advanced/Exercises/02. Distinct Array.js
@@ -1,21 +1,31 @@
 function distinctArray(array) {
     let distincted = array.slice();
+    let result = '';
 
-    for (let j = 0; j < array.length; j++) {
-        let el = array[j];
+    function distinction(array) {
+        for (let j = 0; j < array.length; j++) {
+            let el = array[j];
 
-        for (let i = j + 1; i < distincted.length; i++) {
-            const next = distincted[i];
-            
+            for (let i = j + 1; i < distincted.length; i++) {
+                const next = distincted[i];
+
                 if (el === next) {
                     distincted.splice(i, 1)
-                    
+
                 }
-            
+            }
+
         }
+        return distincted
     }
-    
-    console.log(distincted.join(' '));
+    if (distincted.length < 3) {
+        result = distinction(distincted);
+    } else {
+        result = distinction(distincted);
+    }
+
+    console.log(result.join(' '));
 }
 distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2])
-distinctArray([2, 2, 2, 2])
+distinctArray([2, 2])
+distinctArray([1,2,3,4,5])
---
+++ Arrays Advanced/Exercises/07. Search for a Number.js
@@ -0,0 +1,8 @@
+function searchForANumber(arr, comands) {
+    
+
+
+}
+searchForANumber
+([5, 2, 3, 4, 1, 6], 
+    [5, 2, 3])
