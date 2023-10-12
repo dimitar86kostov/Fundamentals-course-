6d2524a97284d0f2cd5f6aacd48baf6594a494d6
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-10-12 22:27:06 +0300

    100/100

--- Arrays Advanced/Exercises/07. Search for a Number.js
+++ Arrays Advanced/Exercises/07. Search for a Number.js
@@ -1,8 +1,17 @@
 function searchForANumber(arr, comands) {
-    
 
+    let newArr = arr.slice(0, comands[0])
+    newArr.splice(0, comands[1])
+    let count = 0;
+
+    for (let i = 0; i < newArr.length; i++) {
+        if (comands[2] === newArr[i]) {
+            count++;
+        }
+    }
+    console.log(`Number ${comands[2]} occurs ${count} times.`);
 
 }
 searchForANumber
-([5, 2, 3, 4, 1, 6], 
-    [5, 2, 3])
+    ([5, 2, 3, 4, 1, 6, 3],
+        [5, 2, 3])
