46965997d89a669dd25d69fd6b5c0698906c0fb1
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-10-25 00:33:45 +0300

    Ladybugs 30/100

--- Arrays/Exercises/10. Ladybugs.js
+++ Arrays/Exercises/10. Ladybugs.js
@@ -1,47 +1,54 @@
 function ladybugs(params) {
     let num = params.shift();
     let field = [];
-    
-    let initialBugs = params.shift().split(' ').map(Number);
+
+    let bugs = params.shift().split(' ').map(Number);
 
     for (let i = 0; i < num; i++) {
-        if (initialBugs[i] != 0) {
+        if (bugs.includes(i)) {
             field[i] = 1;
-        }else{
+        } else {
             field[i] = 0;
         }
-        
     }
-
-    console.table(field);
 
     for (const el of params) {
         let [start, direction, landing] = el.split(' ');
         start = Number(start);
         landing = Number(landing);
 
-        if (start >= 0 && start < field.length && landing >= 0 && landing < field.length) {
+        if (direction == 'right') {
+            if (start >= 0 && start < field.length && landing >= 0 && landing < field.length) {
 
-            if (field[start] == 1) {
+                if (field[start] == 1) {
 
-                if (start + landing >= field.length) {
-                    field[start] = 0
-                } else {
-                    field[landing] += 1
-                    field[start] = 0
+                    if (start + landing >= field.length) {
+                        field[start] = 0
+                    } else {
+                        field.splice(start, 1, 0);
+                        if (field[landing] == 1) {
+                            field[landing + landing] = 1;
+
+                        }
+                    }
+
                 }
+            }
+        } else {
+            if (start >= 0 && start < field.length && Math.abs(landing) >= 0 && Math.abs(landing) < field.length) {
+                if (field[start] == 1) {
 
-
-
-                if (field[landing] == 2) {
-                    field[landing] -= 1
-                    landing += 1
-                    field[landing] = 1
+                    field.splice(start, 1, 0)
+                    if (landing >= 0) {
+                        field[start - landing] = 1;
+                    } else {
+                        let index = start + landing;
+                        field.splice(index, 1, 1);
+                    }
                 }
             }
         }
     }
-    console.table(field)
     console.log(field.join(' '));
 }
 // ladybugs([3,
@@ -55,3 +62,7 @@ function ladybugs(params) {
 ladybugs([5, '3',
     '3 left 2',
     '1 left -2'])
+ladybugs([5, '3',
+    '3 left 2',
+    '1 left -3',
+])
