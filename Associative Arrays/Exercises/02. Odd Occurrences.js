7d1bba4edf511663c0f36c0803f8f0eb5d5e8709
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-11-06 15:58:04 +0200

    100/100

---
+++ Associative Arrays/Exercises/02. Odd Occurrences.js
@@ -0,0 +1,28 @@
+function oddOccurrences(input) {
+    let map = new Map();
+    let tokens = input.split(' ');
+    let count = 1;
+
+    for (const el of tokens) {
+        let token = el.toLowerCase();
+
+        if (!map.has(token)) {
+            map.set(token, count)
+        } else {
+            let currCount = map.get(token);
+            let newCount = currCount + count;
+            map.set(token, newCount);
+        }
+    }
+    let result = [];
+
+    for (const entry of map) {
+        if (entry[1] % 2 !== 0) {
+
+            result.push(entry[0]);
+        }
+    }
+    console.log(result.join(' '));
+}
+oddOccurrences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
+
