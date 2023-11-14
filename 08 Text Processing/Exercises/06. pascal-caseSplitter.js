d204b66b3b1bbe1929da451df885298712968db9
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-11-14 18:55:09 +0200

    100/100

--- 08 Text Processing/Exercises/06. pascal-caseSplitter.js
+++ 08 Text Processing/Exercises/06. pascal-caseSplitter.js
@@ -6,12 +6,10 @@ function pascelCaseSplitter(str) {
     for (let i = 0; i <= str.length; i++) {
         let char = str[i];
 
-
         if (char.charCodeAt(0) <= 90 && i > 0) {
             arr.push(result);
             result = '';
         }
-
         result += char;
         if (i == str.length - 1) {
             arr.push(result);
