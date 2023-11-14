e93fe70f644e12194a70aa64a87903931b45df95
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-11-14 18:42:20 +0200

    100/100

--- 08 Text Processing/Exercises/06. pascal-caseSplitter.js
+++ 08 Text Processing/Exercises/06. pascal-caseSplitter.js
@@ -6,19 +6,19 @@ function pascelCaseSplitter(str) {
     for (let i = 0; i <= str.length; i++) {
         let char = str[i];
 
-        if (result.length > 1) {
-            if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
-                arr.push(result);
-                result = '';
-            }
+
+        if (char.charCodeAt(0) <= 90 && i > 0) {
+            arr.push(result);
+            result = '';
         }
+
         result += char;
         if (i == str.length - 1) {
             arr.push(result);
             break;
         }
     }
-   console.log(arr.join(', '));
+    console.log(arr.join(', '));
 }
 pascelCaseSplitter('HoldTheDoor')
 pascelCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
