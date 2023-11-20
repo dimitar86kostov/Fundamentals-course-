e4048e5b834e69843a0cc66041d52a976d3cd03e
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-11-20 22:14:15 +0200

    type error

---
+++ 09 RegExp/More Exercises/02-rageQuit.js
@@ -0,0 +1,31 @@
+function rageQuit(input) {
+
+    let wordPattern = /[^0-9]+/g;
+    let digitPattern = /[0-9]+/g;
+    let rage = '';
+    let unique = '';
+
+    let matchChars = input.match(wordPattern);
+    let matchDdigits = input.match(digitPattern);
+    if (matchChars && matchDdigits) {
+        
+        for (let i = 0; i < matchChars.length; i++) {
+            let str = matchChars[i].toString();
+            let result = '';
+            
+            for (const el of str) {
+                let char = el.toUpperCase();
+                result += char;
+                if (!unique.includes(char)) {
+                    unique += char;
+                }
+            }
+            rage += result.repeat(matchDdigits[i])
+        }
+    }
+
+console.log(`Unique symbols used: ${unique.length}`);
+console.log(rage);
+}
+rageQuit('a3')
+rageQuit('aSd2&5s@1')
---
+++ 09 RegExp/More Exercises/03-postOffice.js
@@ -0,0 +1,52 @@
+function postOffice(params) {
+
+    let arr = params.split('|');
+    let mainLetters = '';
+    let secondPart = [];
+    let words = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let msg = arr[i];
+
+        if (i == 0) {
+            let firstPattern = /[#$%*&][A-Z]+[#$%*&]/gm;
+            let match = msg.match(firstPattern);
+            if (match) {
+                let caplLetters = match.toString();
+                mainLetters = caplLetters.slice(1, caplLetters.length - 1);
+            }
+        } else if (i == 1) {
+            let secondPattern = /\d+:\d+/g;
+            let match = msg.match(secondPattern);
+
+            if (match) {
+                for (const el of match) {
+                    let [code, len] = el.split(':');
+                    let currLetter = String.fromCharCode(code);
+                    if (mainLetters.includes(currLetter)) {
+                        secondPart.push(el);
+                    }
+                }
+            }
+        } else if (i == 2) {
+            words = msg.split(' ');
+        }
+    }
+
+    for (const el of secondPart) {
+        let [code, len] = el.split(':');
+        let currLetter = String.fromCharCode(code);
+        len = Number(len) + 1;
+        for (const word of words) {
+            if (word[0] == currLetter && word.length == len) {
+                console.log(word);
+                words.splice(words.indexOf(word), 1)
+                break;
+            }
+        }
+    }
+
+}
+// postOffice('sdsGGasAOTPWEEEdas$AOTP$|a65:1.2s65:03d79:01ds84:02! -80:07++ABs90:1.1|adsaArmyd Gara So La Arm Armyw21 Argo O daOfa Or Ti Sar saTheww The Parahaos')
+postOffice('Urgent"Message.TO$#POAML#|readData79:05:79:0!2reme80:03--23:11{79:05}tak{65:11ar}!77:!23--)77:05ACCSS76:05ad|Remedy Por Ostream :Istream Post sOffices Office Of Ankh-Morpork MR.LIPWIG Mister Lipwig')
+
