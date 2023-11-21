3dd0bffac0906b1828e2f362b49ed20ad1a261b0
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2023-11-21 15:59:32 +0200

    80/100

--- 09 RegExp/More Exercises/03-postOffice.js
+++ 09 RegExp/More Exercises/03-postOffice.js
@@ -1,53 +1,46 @@
-function postOffice(params) {
+function postOffice([params]) {
 
-    let arr = params.split('|');
-    let mainLetters = '';
-    let secondPart = [];
-    let words = [];
+    let [firstPart, secondPart, thirdPart] = params.split('|');
 
-    for (let i = 0; i < arr.length; i++) {
-        let msg = arr[i];
+    let firstPattern = /[#$%*&][A-Z]+[#$%*&]/gm;
+    let secondPattern = /\d+:\d+/g;
+    let caplLetters = '';
+    let validAscii = {};
+    let wordsLen = {};
 
-        if (i == 0) {
-            let firstPattern = /[#$%*&][A-Z]+[#$%*&]/gm;
-            let match = msg.match(firstPattern);
-            if (match) {
-                let caplLetters = match.toString();
-                mainLetters = caplLetters.slice(1, caplLetters.length - 1);
-            }
-        } else if (i == 1) {
-            let secondPattern = /\d+:\d+/g;
-            let match = msg.match(secondPattern);
+    let firstMsg = firstPart.match(firstPattern);
+    let secondMsg = secondPart.match(secondPattern);
+    let thirdMsg = thirdPart.split(' ');
+    thirdMsg.forEach(word => wordsLen[word[0]] = word.length);
 
-            if (match) {
-                for (const el of match) {
-                    let [code, len] = el.split(':');
-                    let currLetter = String.fromCharCode(code);
-                    if (mainLetters.includes(currLetter)) {
-                        secondPart.push(el);
+    if (firstMsg) {
+        let cap = firstMsg.toString();
+        caplLetters = cap.slice(1, cap.length - 1);
+    }
+    if (secondMsg) {
+        for (const capLetter of caplLetters) {
+
+            for (const el of secondMsg) {
+                let [code, len] = el.split(':');
+                len = Number(len) + 1;
+                let codeLetter = String.fromCharCode(code)
+                if (capLetter == codeLetter) {
+
+                    if (codeLetter in wordsLen && wordsLen[codeLetter] == len) {
+                        
+                        validAscii[codeLetter] = len;
+                        secondMsg.splice(secondMsg.indexOf(el), 1);
+                        break;
                     }
                 }
             }
-        } else if (i == 2) {
-            words = msg.split(' ');
         }
     }
-
-    for (const el of secondPart) {
-        let [code, len] = el.split(':');
-        let currLetter = String.fromCharCode(code);
-        len = Number(len) + 1;
-
-        for (const word of words) {
-            if (word[0] == currLetter && word.length == len) {
-                console.log(word);
-                words.splice(words.indexOf(word), 1)
-                break;
-            }
+    for (const word of thirdMsg) {
+        if (word[0] in validAscii && word.length == validAscii[word[0]]) {
+            console.log(word);
         }
     }
-
 }
-// postOffice('sdsGGasAOTPWEEEdas$AOTP$|a65:1.2s65:03d79:01ds84:02! -80:07++ABs90:1.1|adsaArmyd Gara So La Arm Armyw21 Argo O daOfa Or Ti Sar saTheww The Parahaos')
-postOffice('Urgent"Message.TO$#POAML#|readData79:05:79:0!2reme80:03--23:11{79:05}tak{65:11ar}!77:!23--)77:05ACCSS76:05ad|Remedy Por Ostream :Istream Post sOffices Office Of Ankh-Morpork MR.LIPWIG Mister Lipwig')
-
+// postOffice(['sdsGGasAOTPWEEEdas$AOTP$|a65:1.2s65:03d79:01ds84:02! -80:07++ABs90:1.1|adsaArmyd Gara So La Arm Armyw21 Argo O daOfa Or Ti Sar saTheww The Parahaos'])
+postOffice(['Urgent"Message.TO$#POAML#|readData79:05:79:0!2reme80:03--23:11{79:05}tak{65:11ar}!77:!23--)77:05ACCSS76:05ad|Remedy Por Ostream :Istream Post sOffices Office Of Ankh-Morpork MR.LIPWIG Mister Lipwig'])
